6c079e52f037a9487274be815a2b6b5697cf6a00
BoMuShe <3466733324@qq.com>
2023-12-25 00:07:30 +0800

    none

---
+++ app/app/src/main/java/com/bomushe/tlm/Bottom_nav.java
@@ -0,0 +1,14 @@
+package com.bomushe.tlm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bottom_nav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
--- app/app/src/main/java/com/bomushe/tlm/MainActivity.java
+++ app/app/src/main/java/com/bomushe/tlm/MainActivity.java
@@ -2,13 +2,27 @@ package com.bomushe.tlm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //定时跳转
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Bottom_nav.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
